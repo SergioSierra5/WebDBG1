1966de80407e7311f70b70c69d87e30a321dc6a4
Sergio Sierra <sasierram@ut.edu.co>
2022-07-30 19:05:52 -0500

    Aqui se actualiza la Base de Datos

---
+++ WebDBG1/Areas/Identity/Departamento.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace WebDBG1.Areas.Identity
+{
+    public class Departamento
+    {
+        public int ID { get; set; }
+
+        [MaxLength(40)]
+
+        public string? Nombre { get; set; }
+
+    }
+}
